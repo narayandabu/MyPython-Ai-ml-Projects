3975c498677a463e038cce34943a671d17978930
Narayan <narayandabu123@gmail.com>
2023-10-10 20:10:58 +0530

    Created and Exported a Pipeline for training and testing the model

--- Sentiment_Analysis/Interface for testing the trained Model.py
+++ Sentiment_Analysis/Interface for testing the trained Model.py
@@ -1,16 +1,7 @@
 from joblib import load
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
 
 vocabulary = load('voca.joblib')
-
-
-def covert_to_tfdif(data):
-    count_vect = CountVectorizer(vocabulary=vocabulary)
-    tfidf_transformer = TfidfTransformer()
-    tfidf = count_vect.fit_transform(data)
-    tfidf = tfidf_transformer.fit_transform(tfidf)
-    return tfidf
+my_pipeline = load('pipeline.joblib')
 
 
 def sol_transform(solns):
@@ -26,9 +17,9 @@ def sol_transform(solns):
 model = load('model.joblib')
 input_data = ['hellow', 'bad', 'struggle', 'lose']  # could be anything or any form
 
-refined_data = covert_to_tfdif(input_data)
+refined_data = my_pipeline.transform(input_data)
 predictions = model.predict(refined_data)
 
 predictions = sol_transform(predictions)
 
-print(list(predictions))
+print(predictions)
--- Sentiment_Analysis/sentiment_analysis_model_train.py
+++ Sentiment_Analysis/sentiment_analysis_model_train.py
@@ -6,38 +6,15 @@ from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score
 from joblib import dump
+from sklearn.pipeline import Pipeline
 
-df = pd.read_csv('./Data/cleandata.csv')
-df = df.drop(['Tweets', 'Date'], axis=1)
 pos_neg_data = pd.read_excel("./Data/LabeledText.xlsx", engine="openpyxl")
-df1 = df.copy(deep=True)
 
+my_pipeline = Pipeline(steps=[
+    ('count_vect', CountVectorizer()),
+    ('tfidf', TfidfTransformer()),
+])
 
-def normalize_data(data):
-    return (data - np.min(data)) / (np.max(data) - np.min(data))
-
-
-def split_single(data):
-    for tweet in data:
-        x = tweet
-        if len(str(x).split()) > 1:
-            df.drop(df[df['Cleaned_Tweets'] == tweet].index, inplace=True)
-
-
-def remove_some(data):
-    x = data.split()
-    new_str = ""
-    for i in x:
-        if i[0] == '#':
-            new_str = new_str + ' ' + i[1:len(i)]
-        else:
-            new_str = new_str + ' ' + i
-    return new_str
-
-
-for data in pos_neg_data['Caption']:
-    trimmed_data = remove_some(data)
-    pos_neg_data['Caption'].replace(data, trimmed_data, inplace=True)
 
 labels = {
     'positive': 1.0,
@@ -52,25 +29,17 @@ y = np.array(pos_neg_data['LABEL'])
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20, random_state=42)
 
-count_vect = CountVectorizer()
-tfidf_transformer = TfidfTransformer()
-
-x_train_tfidf = count_vect.fit_transform(x_train)
-x_train_tfidf = tfidf_transformer.fit_transform(x_train_tfidf)
-
-x_test_tfidf = count_vect.transform(x_test)
-x_test_tfidf = tfidf_transformer.transform(x_test_tfidf)
-
-vocabulary = count_vect.get_feature_names_out()
-print(vocabulary)
-
-print(x_train_tfidf.shape, x_test_tfidf.shape)
+x_train_tfidf = my_pipeline.fit_transform(x_train)
+x_test_tfidf = my_pipeline.transform(x_test)
+vocabulary = my_pipeline.get_feature_names_out()
 
 model = MultinomialNB()
 model.fit(x_train_tfidf, y_train)
-
 predicted = model.predict(x_test_tfidf)
 score = accuracy_score(y_test, predicted)
+
 dump(model, 'model.joblib')
-dump(vocabulary,'voca.joblib')
+dump(vocabulary, 'voca.joblib')
+dump(my_pipeline,'pipeline.joblib')
+
 print("Score:- ", score * 100, "%")
